b1911a09a8607ee8508de8c2f68c5c7539cc5492
Prithijit <prithijitm@gmail.com>
2020-12-07 17:57:21 +0530

    GoodCommit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const Common_1 = require("Common");
+const Common_1 = require("./Common");
 class MainClass {
     static main() {
         Common_1.LogManager.info("Welcome to the world of MERN !!! ");
--- index.ts
+++ index.ts
@@ -1,7 +1,3 @@
-import { LogManager } from "Common";
-
-
-
 import { LogManager } from "./Common";
 
 class MainClass {
